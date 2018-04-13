69330db869cbd372c4c1fb44ec2a45e6059f423a
Efrain Perez Jr <efrainperezjr@live.com>
2018-04-13 16:41:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ var customerName = 'bob';
 function upperCaseCustomerName() {
   customerName = customerName[0].toUpperCase() + customerName.slice(1);
 }
+
+function setBestCustomer() {
+  var bestCustomer = 'not bob';
+}
