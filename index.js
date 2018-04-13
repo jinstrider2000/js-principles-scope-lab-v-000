8a2cfc4e7af185d41e2ed4424492f3891fd036b7
Efrain Perez Jr <efrainperezjr@live.com>
2018-04-13 16:39:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName[0].toUpperCase() + customerName.slice(1);
+}
+
+(function() {
+  'use strict';
+
+}());
